e168f3b7087f6a342112a02295b1bcb59392f740
sunny <anna7789@naver.com>
2025-12-17 16:56:43 +0900

    test: 보안 의존성 목 처리로 컨텍스트 로드 안정화

--- src/test/java/com/cubeai/CubeaiApplicationTests.java
+++ src/test/java/com/cubeai/CubeaiApplicationTests.java
@@ -1,10 +1,40 @@
 package com.cubeai;
 
+import com.cubeai.domain.auth.service.CustomOAuth2UserService;
+import com.cubeai.global.jwt.JwtAuthenticationEntryPoint;
+import com.cubeai.global.jwt.JwtProvider;
+import com.cubeai.global.security.CustomOAuth2SuccessHandler;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 
 @SpringBootTest
+@SuppressWarnings("removal")
 class CubeaiApplicationTests {
+
+	@MockBean
+	private JwtProvider jwtProvider;
+
+	@MockBean
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+	@MockBean
+	private CustomOAuth2UserService customOAuth2UserService;
+
+	@MockBean
+	private CustomOAuth2SuccessHandler customOAuth2SuccessHandler;
+
+	@MockBean
+	private ClientRegistrationRepository clientRegistrationRepository;
+
+	@MockBean
+	private OAuth2AuthorizedClientService authorizedClientService;
+
+	@MockBean
+	private OAuth2AuthorizedClientRepository authorizedClientRepository;
 
 	@Test
 	void contextLoads() {
